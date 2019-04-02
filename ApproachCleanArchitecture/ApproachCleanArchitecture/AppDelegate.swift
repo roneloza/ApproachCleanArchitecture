1f7f3d385e8fef4e779fb6db121a805eed62518e
Rone Loza <rone.loza@mambo.pe>
2019-04-02 18:52:04 -0500

    Fabric Beta version: 1.0

--- ApproachCleanArchitecture/ApproachCleanArchitecture/AppDelegate.swift
+++ ApproachCleanArchitecture/ApproachCleanArchitecture/AppDelegate.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import Firebase
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        Fabric.with([Crashlytics.self])
+
         return true
     }
 
